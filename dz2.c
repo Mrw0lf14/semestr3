84f2bb5fc0f0fc2210ffad90bb1ad8a8fac91766
Mrw0lf14 <43212019+Mrw0lf14@users.noreply.github.com>
2020-10-07 20:26:28 +0300

    Update dz2.c

--- dz2.c
+++ dz2.c
@@ -2,108 +2,106 @@
 #include <stdlib.h>
 #include <string.h>
 
+int** arr = NULL;
 
-void show(int arr[], int N);
-void createDot(int arr[], int N);
-void init_points(int arr[], int N);
-void show_points(int N);
-
-int** points = NULL;
+void show(int N);
+void check(int* viseted, int cur, int find, int* bol);
+void create_dot(int N);
 
 int main(void)
 {
     int N = 0;
-    int* arr = NULL;
+    
     FILE* f = NULL;
     
     if((f = fopen("data.txt", "r")) != NULL){
 
         fscanf(f, "%d", &N);
-        points = (int**)malloc(N*sizeof(int*));
-        arr = (int*)calloc(sizeof(int), N*N);
+
+        arr = (int**)calloc(sizeof(int*), N);
+        for(int i = 0; i < N; i++){
+            arr[i] = (int*)malloc(sizeof(int));
+            arr[i][0] = -1;
+        }
+        
+        int c = 0;
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
                 int tmp = 0;
                 fscanf(f, "%d", &tmp);
-                arr[i+j*N] = tmp;
+                if(tmp == 1){
+                    arr[i][c] = j;
+                    c++;
+                    arr[i] = (int*)realloc(arr[i], sizeof(int)*(c+1));
+                }
             }
+            arr[i][c] = -1;
+            c = 0;
         }
     }
     else
         printf("File data.txt did not open :(\n");
     
-    init_points(arr, N);
-    show_points(N);
-    show(arr,N);
-    fclose(f);
-    createDot(arr, N);
-    scanf("%d", &N);
+    int bol = 0;
     
+    for(int i = 0; i < N; i++){
+        int* visited = (int*)calloc(N, sizeof(int));
+        check(visited, 0, i, &bol);
+        if(bol == 0)
+            printf("graph is bad\n");
+        bol = 0; 
+        free(visited);
+    }
+    
+        
+    create_dot(N);
+    show(N);
+    free(arr);
+    getchar();  
     
     return 0;
 }
 
-void show(int arr[], int N){
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            printf("%d ", arr[i+j*N]);
+void show(int N){
+    for (int i = 0; i < N; i++){
+        for(int j = 0; arr[i][j] != -1; j++){
+            printf("%d ", arr[i][j]);
         }
         printf("\n");
     }
 }
 
-void createDot(int arr[], int N){
-    
+void check(int* viseted, int cur, int find, int* bol){
+    if(viseted[cur] == 1) 
+        return;
+
+    viseted[cur] = 1;
+
+    if(cur == find){
+        *bol = 1;
+    }
+    else{
+        for(int i = 0; arr[cur][i] != -1; i++){
+            check(viseted, arr[cur][i], find, bol);
+        }
+    }
+}
+
+void create_dot(int N){
     FILE* f = NULL;
+
     if((f = fopen("out.dot", "w")) != NULL){
         fprintf(f, "digraph graph {\n");
-        for(int j = 0; j < N; j++){
-            for(int i = 0; i < N; i++){
-                if(i == j)
-                    fprintf(f, "%c ", (char)('a'+i));
-                else if(arr[i+j*N] == 1 && i > j){
-                    fprintf(f, "-- %c", (char)('a'+i));
-                }
+        for(int i = 0; i < N; i++){
+            fprintf(f, "%c ", ('a'+i));
+            for(int j = 0; arr[i][j] != -1; j++){
+                fprintf(f, "-- %c", ('a'+arr[i][j]));
             }
             fprintf(f, ";\n");
         }
         fprintf(f, "}");
+        fclose(f);
     }
     else
-        printf("file out.dot did not open :(");
-    fclose(f);
-}
-
-void init_points(int arr[], int N){
-    
-    int c = 1;
-    
-    for(int j = 0; j < N; j++){
-
-        int* tp = (int*)malloc(sizeof(int));
-
-        for(int i = 0; i < N; i++){
-            if(arr[i+j*N] == 1 && i != j){
-                tp = (int*)realloc(tp, c+1);
-                tp[c-1] = i;
-                c++;
-            }
-        }
-        tp = (int*)realloc(tp, c+1);
-        tp[c] = -1;
-        c = 0;
-        *(points+j) = tp;
-    }
-}
-
-void show_points(int N){
-    int* tp = NULL;
-    for(int i = 0; i < N; i++){
-        printf("%d:", i);
-        tp = *(points+i);
-        for(int j = 0; tp[j] != -1; j++){
-            printf(" %d", tp[j]);
-        }
-        printf("\n");
-    }
+        printf("dot cannpt be open\n");
 }
