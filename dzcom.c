bd1c4991abdbaf3920ba56a57521ce4037a901cd
Mrw0lf14 <43212019+Mrw0lf14@users.noreply.github.com>
2020-11-17 22:42:21 -0800

    Update dzcom.c

--- dzcom.c
+++ dzcom.c
@@ -71,7 +71,7 @@ void push_arr(long int** arr, int size, int max){
 
 void show_arr(long int* arr, int size){
     for(int i = 0; i < size; i++){
-        printf("%d ", arr[i]);
+        printf("%ld ", arr[i]);
     }
     printf("\n");
 }
